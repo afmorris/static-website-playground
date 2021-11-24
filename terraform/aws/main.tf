e073f94b16a6d3063ad5fc317d0f5d2e31b7df41
Tony Morris <afmorris@users.noreply.github.com>
2021-11-24 08:10:30 -0500

    Bump provider versions (#1)
    * Bump provider versions
    
    * Update terraform lock file

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.63"
+      version = "~> 3.66"
     }
 
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 3.0"
+      version = "~> 3.4"
     }
   }
 
